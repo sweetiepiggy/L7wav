db033d311ba2210ad14b3047ad24b470e0b6f7e6
Sweetie Piggy <sweetiepiggyapps@gmail.com>
2012-03-03 22:24:57 +0800

    add a through g buttons

--- src/com/sweetiepiggy/l7wav/L7wavActivity.java
+++ src/com/sweetiepiggy/l7wav/L7wavActivity.java
@@ -26,35 +26,104 @@ import android.media.AudioTrack;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class L7wavActivity extends Activity
 {
+	int minSize = AudioTrack.getMinBufferSize(44100, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT);
+	AudioTrack track = new AudioTrack(AudioManager.STREAM_MUSIC, 44100,
+		AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT,
+		minSize, AudioTrack.MODE_STREAM);
+	short[] buf = new short[20480];
+
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
+		track.play();
+
 		Button a_button = (Button)findViewById(R.id.a_button);
 		a_button.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				Toast.makeText(getApplicationContext(), "a",
-						Toast.LENGTH_SHORT).show();
-				int minSize = AudioTrack.getMinBufferSize(44100, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT);
-				AudioTrack track = new AudioTrack(AudioManager.STREAM_MUSIC, 44100,
-					AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT,
-					minSize, AudioTrack.MODE_STREAM);
-				track.play();
-				short[] buf = new short[20480];
 				float angle = 0;
 				for (int i=0; i < 20480; ++i) {
 					buf[i] = (short)(Math.sin(angle)*Short.MAX_VALUE);
 					angle += (float)(2*Math.PI) * 440 / 44100;
 				}
 				track.write(buf, 0 , 20480);
-				Toast.makeText(getApplicationContext(), "b",
-						Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		Button b_button = (Button)findViewById(R.id.b_button);
+		b_button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				float angle = 0;
+				for (int i=0; i < 20480; ++i) {
+					buf[i] = (short)(Math.sin(angle)*Short.MAX_VALUE);
+					angle += (float)(2*Math.PI) * 493.883 / 44100;
+				}
+				track.write(buf, 0 , 20480);
+			}
+		});
+
+		Button c_button = (Button)findViewById(R.id.c_button);
+		c_button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				float angle = 0;
+				for (int i=0; i < 20480; ++i) {
+					buf[i] = (short)(Math.sin(angle)*Short.MAX_VALUE);
+					angle += (float)(2*Math.PI) * 523.251 / 44100;
+				}
+				track.write(buf, 0 , 20480);
+			}
+		});
+
+		Button d_button = (Button)findViewById(R.id.d_button);
+		d_button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				float angle = 0;
+				for (int i=0; i < 20480; ++i) {
+					buf[i] = (short)(Math.sin(angle)*Short.MAX_VALUE);
+					angle += (float)(2*Math.PI) * 587.330 / 44100;
+				}
+				track.write(buf, 0 , 20480);
+			}
+		});
+
+		Button e_button = (Button)findViewById(R.id.e_button);
+		e_button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				float angle = 0;
+				for (int i=0; i < 20480; ++i) {
+					buf[i] = (short)(Math.sin(angle)*Short.MAX_VALUE);
+					angle += (float)(2*Math.PI) * 659.255 / 44100;
+				}
+				track.write(buf, 0 , 20480);
+			}
+		});
+
+		Button f_button = (Button)findViewById(R.id.f_button);
+		f_button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				float angle = 0;
+				for (int i=0; i < 20480; ++i) {
+					buf[i] = (short)(Math.sin(angle)*Short.MAX_VALUE);
+					angle += (float)(2*Math.PI) * 698.456 / 44100;
+				}
+				track.write(buf, 0 , 20480);
+			}
+		});
+
+		Button g_button = (Button)findViewById(R.id.g_button);
+		g_button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				float angle = 0;
+				for (int i=0; i < 20480; ++i) {
+					buf[i] = (short)(Math.sin(angle)*Short.MAX_VALUE);
+					angle += (float)(2*Math.PI) * 783.991 / 44100;
+				}
+				track.write(buf, 0 , 20480);
 			}
 		});
 	}
