4bc70c3167ce989f7aac4171bde9c7493837605e
Sweetie Piggy <sweetiepiggyapps@gmail.com>
2013-01-09 22:27:35 +0800

    added octaves

--- src/com/sweetiepiggy/l7wav/L7wavActivity.java
+++ src/com/sweetiepiggy/l7wav/L7wavActivity.java
@@ -49,6 +49,27 @@ public class L7wavActivity extends Activity
 	short[] mSnd_e = new short[SAMPLE_SIZE];
 	short[] mSnd_f = new short[SAMPLE_SIZE];
 	short[] mSnd_g = new short[SAMPLE_SIZE];
+	short[] mSnd_a2 = new short[SAMPLE_SIZE];
+	short[] mSnd_b2 = new short[SAMPLE_SIZE];
+	short[] mSnd_c2 = new short[SAMPLE_SIZE];
+	short[] mSnd_d2 = new short[SAMPLE_SIZE];
+	short[] mSnd_e2 = new short[SAMPLE_SIZE];
+	short[] mSnd_f2 = new short[SAMPLE_SIZE];
+	short[] mSnd_g2 = new short[SAMPLE_SIZE];
+	short[] mSnd_a3 = new short[SAMPLE_SIZE];
+	short[] mSnd_b3 = new short[SAMPLE_SIZE];
+	short[] mSnd_c3 = new short[SAMPLE_SIZE];
+	short[] mSnd_d3 = new short[SAMPLE_SIZE];
+	short[] mSnd_e3 = new short[SAMPLE_SIZE];
+	short[] mSnd_f3 = new short[SAMPLE_SIZE];
+	short[] mSnd_g3 = new short[SAMPLE_SIZE];
+	short[] mSnd_a4 = new short[SAMPLE_SIZE];
+	short[] mSnd_b4 = new short[SAMPLE_SIZE];
+	short[] mSnd_c4 = new short[SAMPLE_SIZE];
+	short[] mSnd_d4 = new short[SAMPLE_SIZE];
+	short[] mSnd_e4 = new short[SAMPLE_SIZE];
+	short[] mSnd_f4 = new short[SAMPLE_SIZE];
+	short[] mSnd_g4 = new short[SAMPLE_SIZE];
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -70,6 +91,27 @@ public class L7wavActivity extends Activity
 		init_l7wav(mSnd_e, FREQ_E);
 		init_l7wav(mSnd_f, FREQ_F);
 		init_l7wav(mSnd_g, FREQ_G);
+		init_l7wav(mSnd_a2, FREQ_A * 2);
+		init_l7wav(mSnd_b2, FREQ_B * 2);
+		init_l7wav(mSnd_c2, FREQ_C * 2);
+		init_l7wav(mSnd_d2, FREQ_D * 2);
+		init_l7wav(mSnd_e2, FREQ_E * 2);
+		init_l7wav(mSnd_f2, FREQ_F * 2);
+		init_l7wav(mSnd_g2, FREQ_G * 2);
+		init_l7wav(mSnd_a3, FREQ_A * 4);
+		init_l7wav(mSnd_b3, FREQ_B * 4);
+		init_l7wav(mSnd_c3, FREQ_C * 4);
+		init_l7wav(mSnd_d3, FREQ_D * 4);
+		init_l7wav(mSnd_e3, FREQ_E * 4);
+		init_l7wav(mSnd_f3, FREQ_F * 4);
+		init_l7wav(mSnd_g3, FREQ_G * 4);
+		init_l7wav(mSnd_a4, FREQ_A * 8);
+		init_l7wav(mSnd_b4, FREQ_B * 8);
+		init_l7wav(mSnd_c4, FREQ_C * 8);
+		init_l7wav(mSnd_d4, FREQ_D * 8);
+		init_l7wav(mSnd_e4, FREQ_E * 8);
+		init_l7wav(mSnd_f4, FREQ_F * 8);
+		init_l7wav(mSnd_g4, FREQ_G * 8);
 	}
 
 	void init_buttons()
@@ -81,6 +123,27 @@ public class L7wavActivity extends Activity
 		init_button(R.id.e_button, mSnd_e);
 		init_button(R.id.f_button, mSnd_f);
 		init_button(R.id.g_button, mSnd_g);
+		init_button(R.id.a2_button, mSnd_a2);
+		init_button(R.id.b2_button, mSnd_b2);
+		init_button(R.id.c2_button, mSnd_c2);
+		init_button(R.id.d2_button, mSnd_d2);
+		init_button(R.id.e2_button, mSnd_e2);
+		init_button(R.id.f2_button, mSnd_f2);
+		init_button(R.id.g2_button, mSnd_g2);
+		init_button(R.id.a3_button, mSnd_a3);
+		init_button(R.id.b3_button, mSnd_b3);
+		init_button(R.id.c3_button, mSnd_c3);
+		init_button(R.id.d3_button, mSnd_d3);
+		init_button(R.id.e3_button, mSnd_e3);
+		init_button(R.id.f3_button, mSnd_f3);
+		init_button(R.id.g3_button, mSnd_g3);
+		init_button(R.id.a4_button, mSnd_a4);
+		init_button(R.id.b4_button, mSnd_b4);
+		init_button(R.id.c4_button, mSnd_c4);
+		init_button(R.id.d4_button, mSnd_d4);
+		init_button(R.id.e4_button, mSnd_e4);
+		init_button(R.id.f4_button, mSnd_f4);
+		init_button(R.id.g4_button, mSnd_g4);
 	}
 
 	void init_button(int button_id, final short[] buf)
@@ -118,7 +181,7 @@ public class L7wavActivity extends Activity
 		int j = 0;
 		for (int i=0; i < SAMPLE_SIZE; ++i, ++j) {
 			buf[i] = x;
-			if (j == (int) (SAMPLE_RATE / (freq / 1.))) {
+			if (j == (int) (SAMPLE_RATE / (freq / 4.))) {
 				j = 0;
 				pos = !pos;
 				x = pos ? Short.MAX_VALUE : Short.MIN_VALUE;
