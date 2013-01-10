7bad0a33a6bdb7271c35d38ac84f4cf2963a256e
Sweetie Piggy <sweetiepiggyapps@gmail.com>
2013-01-10 21:56:33 +0800

    create layout programmatically

--- src/com/sweetiepiggy/l7wav/L7wavActivity.java
+++ src/com/sweetiepiggy/l7wav/L7wavActivity.java
@@ -24,8 +24,11 @@ import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 public class L7wavActivity extends Activity
 {
@@ -41,35 +44,6 @@ public class L7wavActivity extends Activity
 	static final double FREQ_G =  783.991;
 
 	AudioTrack mTrack;
-	short[] buf = new short[SAMPLE_SIZE];
-	short[] mSnd_a = new short[SAMPLE_SIZE];
-	short[] mSnd_b = new short[SAMPLE_SIZE];
-	short[] mSnd_c = new short[SAMPLE_SIZE];
-	short[] mSnd_d = new short[SAMPLE_SIZE];
-	short[] mSnd_e = new short[SAMPLE_SIZE];
-	short[] mSnd_f = new short[SAMPLE_SIZE];
-	short[] mSnd_g = new short[SAMPLE_SIZE];
-	short[] mSnd_a2 = new short[SAMPLE_SIZE];
-	short[] mSnd_b2 = new short[SAMPLE_SIZE];
-	short[] mSnd_c2 = new short[SAMPLE_SIZE];
-	short[] mSnd_d2 = new short[SAMPLE_SIZE];
-	short[] mSnd_e2 = new short[SAMPLE_SIZE];
-	short[] mSnd_f2 = new short[SAMPLE_SIZE];
-	short[] mSnd_g2 = new short[SAMPLE_SIZE];
-	short[] mSnd_a3 = new short[SAMPLE_SIZE];
-	short[] mSnd_b3 = new short[SAMPLE_SIZE];
-	short[] mSnd_c3 = new short[SAMPLE_SIZE];
-	short[] mSnd_d3 = new short[SAMPLE_SIZE];
-	short[] mSnd_e3 = new short[SAMPLE_SIZE];
-	short[] mSnd_f3 = new short[SAMPLE_SIZE];
-	short[] mSnd_g3 = new short[SAMPLE_SIZE];
-	short[] mSnd_a4 = new short[SAMPLE_SIZE];
-	short[] mSnd_b4 = new short[SAMPLE_SIZE];
-	short[] mSnd_c4 = new short[SAMPLE_SIZE];
-	short[] mSnd_d4 = new short[SAMPLE_SIZE];
-	short[] mSnd_e4 = new short[SAMPLE_SIZE];
-	short[] mSnd_f4 = new short[SAMPLE_SIZE];
-	short[] mSnd_g4 = new short[SAMPLE_SIZE];
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -77,73 +51,69 @@ public class L7wavActivity extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		init_snd_arrays();
-		init_buttons();
+		create_row(7);
+		create_row(6);
+		create_row(5);
+		create_row(4);
+		create_row(3);
+		create_row(2);
+		create_row(1);
+		create_row(0);
+
 		init_audio_track();
 	}
 
-	void init_snd_arrays()
+	void create_row(int octave)
 	{
-		init_l7wav(mSnd_a, FREQ_A);
-		init_l7wav(mSnd_b, FREQ_B);
-		init_l7wav(mSnd_c, FREQ_C);
-		init_l7wav(mSnd_d, FREQ_D);
-		init_l7wav(mSnd_e, FREQ_E);
-		init_l7wav(mSnd_f, FREQ_F);
-		init_l7wav(mSnd_g, FREQ_G);
-		init_l7wav(mSnd_a2, FREQ_A * 2);
-		init_l7wav(mSnd_b2, FREQ_B * 2);
-		init_l7wav(mSnd_c2, FREQ_C * 2);
-		init_l7wav(mSnd_d2, FREQ_D * 2);
-		init_l7wav(mSnd_e2, FREQ_E * 2);
-		init_l7wav(mSnd_f2, FREQ_F * 2);
-		init_l7wav(mSnd_g2, FREQ_G * 2);
-		init_l7wav(mSnd_a3, FREQ_A * 4);
-		init_l7wav(mSnd_b3, FREQ_B * 4);
-		init_l7wav(mSnd_c3, FREQ_C * 4);
-		init_l7wav(mSnd_d3, FREQ_D * 4);
-		init_l7wav(mSnd_e3, FREQ_E * 4);
-		init_l7wav(mSnd_f3, FREQ_F * 4);
-		init_l7wav(mSnd_g3, FREQ_G * 4);
-		init_l7wav(mSnd_a4, FREQ_A * 8);
-		init_l7wav(mSnd_b4, FREQ_B * 8);
-		init_l7wav(mSnd_c4, FREQ_C * 8);
-		init_l7wav(mSnd_d4, FREQ_D * 8);
-		init_l7wav(mSnd_e4, FREQ_E * 8);
-		init_l7wav(mSnd_f4, FREQ_F * 8);
-		init_l7wav(mSnd_g4, FREQ_G * 8);
+		LinearLayout top_layout = (LinearLayout) findViewById(R.id.top_layout);
+
+		LinearLayout row_layout = new LinearLayout(this);
+		row_layout.setOrientation(LinearLayout.HORIZONTAL);
+		row_layout.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
+					LayoutParams.WRAP_CONTENT));
+		row_layout.setGravity(Gravity.CENTER);
+		row_layout.setPadding(100, 10, 100, 10);
+
+		double octave_multiplier = Math.pow(2, octave);
+
+		Button a_button = new Button(this);
+		init_button(a_button, FREQ_A * octave_multiplier, 1f);
+		row_layout.addView(a_button);
+		Button b_button = new Button(this);
+		init_button(b_button, FREQ_B * octave_multiplier, 2f);
+		row_layout.addView(b_button);
+		Button c_button = new Button(this);
+		init_button(c_button, FREQ_C * octave_multiplier, 3f);
+		row_layout.addView(c_button);
+		Button d_button = new Button(this);
+		init_button(d_button, FREQ_D * octave_multiplier, 1f);
+		row_layout.addView(d_button);
+		Button e_button = new Button(this);
+		init_button(e_button, FREQ_E * octave_multiplier, 2f);
+		row_layout.addView(e_button);
+		Button f_button = new Button(this);
+		init_button(f_button, FREQ_F * octave_multiplier, 1f);
+		row_layout.addView(f_button);
+		Button g_button = new Button(this);
+		init_button(g_button, FREQ_G * octave_multiplier, 2f);
+		row_layout.addView(g_button);
+
+		top_layout.addView(row_layout);
 	}
 
-	void init_buttons()
+	void init_button(Button button, final double freq, float weight)
 	{
-		init_button(R.id.a_button, mSnd_a);
-		init_button(R.id.b_button, mSnd_b);
-		init_button(R.id.c_button, mSnd_c);
-		init_button(R.id.d_button, mSnd_d);
-		init_button(R.id.e_button, mSnd_e);
-		init_button(R.id.f_button, mSnd_f);
-		init_button(R.id.g_button, mSnd_g);
-		init_button(R.id.a2_button, mSnd_a2);
-		init_button(R.id.b2_button, mSnd_b2);
-		init_button(R.id.c2_button, mSnd_c2);
-		init_button(R.id.d2_button, mSnd_d2);
-		init_button(R.id.e2_button, mSnd_e2);
-		init_button(R.id.f2_button, mSnd_f2);
-		init_button(R.id.g2_button, mSnd_g2);
-		init_button(R.id.a3_button, mSnd_a3);
-		init_button(R.id.b3_button, mSnd_b3);
-		init_button(R.id.c3_button, mSnd_c3);
-		init_button(R.id.d3_button, mSnd_d3);
-		init_button(R.id.e3_button, mSnd_e3);
-		init_button(R.id.f3_button, mSnd_f3);
-		init_button(R.id.g3_button, mSnd_g3);
-		init_button(R.id.a4_button, mSnd_a4);
-		init_button(R.id.b4_button, mSnd_b4);
-		init_button(R.id.c4_button, mSnd_c4);
-		init_button(R.id.d4_button, mSnd_d4);
-		init_button(R.id.e4_button, mSnd_e4);
-		init_button(R.id.f4_button, mSnd_f4);
-		init_button(R.id.g4_button, mSnd_g4);
+		button.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+					100, weight));
+
+		final short[] snd = new short[SAMPLE_SIZE];
+		init_l7wav(snd, freq);
+
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				play_sound(snd);
+			}
+		});
 	}
 
 	void init_button(int button_id, final short[] buf)
@@ -181,7 +151,7 @@ public class L7wavActivity extends Activity
 		int j = 0;
 		for (int i=0; i < SAMPLE_SIZE; ++i, ++j) {
 			buf[i] = x;
-			if (j == (int) (SAMPLE_RATE / (freq / 4.))) {
+			if (j == (int) (SAMPLE_RATE / (freq / 8.))) {
 				j = 0;
 				pos = !pos;
 				x = pos ? Short.MAX_VALUE : Short.MIN_VALUE;
@@ -193,7 +163,7 @@ public class L7wavActivity extends Activity
 		float angle = 0;
 		for (int i=0; i < SAMPLE_SIZE; ++i) {
 			buf[i] = (short)(Math.sin(angle)*Short.MAX_VALUE);
-			angle += (float)(2*Math.PI) * (freq / 2.) / SAMPLE_RATE;
+			angle += (float)(2*Math.PI) * (freq / 4.) / SAMPLE_RATE;
 		}
 	}
 
